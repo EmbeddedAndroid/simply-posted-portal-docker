e0d15517d107b4700cef1625698bf6c37901eea1
Tyler Baker <tyler.baker@linaro.org>
2017-01-11 17:30:51 -0800

    update serve script
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

--- serve.sh
+++ serve.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cd /root
+mkdir -p /webapps
+cd /webapps
 git clone  https://github.com/EmbeddedAndroid/simply-posted-portal.git
 cd simply-posted-portal && \
+groupadd --system webapps
+useradd --system --gid webapps --shell /bin/bash --home /webapps/simply-posted-portal simplyposted
+chown -Rv simplyposted /webapps/simply-posted-portal/
 pip install -r requirements.txt
 ./manage.py migrate
 ./manage.py collectstatic --noinput
@@ -9,4 +13,9 @@ pip install -r requirements.txt
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('$USERNAME', '$EMAIL', '$PASSWORD')" | python manage.py shell
 ./manage.py sync_plans
 ./manage.py sync_customers
-./manage.py runserver 0.0.0.0:8000
+if [ "$DEPLOYMENT" = "production" ];
+then
+	gunicorn simply_posted_portal.wsgi:application --user=simplyposted --group=webapps --workers=17 --log-level=debug --bind=0.0.0.0:8000
+else
+	./manage.py runserver 0.0.0.0:8000
+fi
